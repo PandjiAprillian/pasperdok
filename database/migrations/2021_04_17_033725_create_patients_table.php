6380119c24901d7f916f8598eb1fe6e21f28b520
PandjiAprillian <pandjiaprilian@outlook.com>
2021-04-18 16:12:42 +0700

    menambahkan script untuk mengambil nama foto yang diupload user

--- database/migrations/2021_04_17_033725_create_patients_table.php
+++ database/migrations/2021_04_17_033725_create_patients_table.php
@@ -24,7 +24,7 @@ class CreatePatientsTable extends Migration
             $table->string('photo')->default('default_profile.jpg');
             $table->text('keluhan');
             $table->tinyInteger('rawat_inap')->default(0);
-            $table->foreignId('room_id')->constrained()->onDelete('cascade')->default(0);
+            $table->foreignId('room_id')->default(0)->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+$('input[type=file]').on('change', function () {
+    let filenames = [];
+    let files = document.getElementById('photo').files;
+
+    for(let i in files) {
+        if (files.hasOwnProperty(i)) {
+            filenames.push(files[i].name);
+        }
+    }
+
+    $(this).next('.custom-file-label').addClass('selected').html(filenames.join(', '));
+});
