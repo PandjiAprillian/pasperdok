7a213517da501fa8dbfc10cf6a590abd996752f0
PandjiAprillian <pandjiaprilian@outlook.com>
2021-04-27 19:27:04 +0700

    mengubah PatientSeeder

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -19,7 +19,7 @@ class PatientSeeder extends Seeder
 
         $rafita = \App\Models\User::create(
             [
-                'nama'              => 'Rafita Suci',
+                'nama'              => 'Rafita Putri Nuraeni Suci',
                 'email'             => 'rafita@test.com',
                 'email_verified_at' => now(),
                 'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
